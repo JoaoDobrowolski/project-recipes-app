d155cb406fcf56fe2089f93b33e0708f9a22ed6a
Isabelle Strojake <strojake@gmail.com>
2022-08-29 21:12:53 -0300

    done recipes

--- src/componentes/DoneRecipesCards.js
+++ src/componentes/DoneRecipesCards.js
@@ -1,60 +1,104 @@
-import React from 'react';
+import { Link } from 'react-router-dom';
+import PropTypes from 'prop-types';
+import React, { useState } from 'react';
 import shareIcon from '../images/shareIcon.svg';
+import '../styles/DoneRecipesCards.css';
 
-function CardDoneRecipesCards() {
-  const doneRecipes = localStorage.getItem('doneRecipes')
-    ? JSON.parse(localStorage.getItem('doneRecipes'))
-    : [];
+const copy = require('clipboard-copy');
+// Retirado de https://www.npmjs.com/package/clipboard-copy
+
+function DoneRecipesCards({ doneRecipes }) {
+  const [copiedLink, copiedLinkset] = useState(false);
+
+  const num = 2;
+
+  const getURL = (item) => {
+    const url = `http://localhost:3000/${item.type}s/${item.id}`;
+
+    copy(url);
+
+    copiedLinkset(true);
+  };
 
   return (
     <section>
-      {doneRecipes.map((recipe, index) => (
-        <div key={ index }>
-          {
-            recipe.type === 'food' ? (
-              <p data-testid={ `${index}-horizontal-top-text` }>
-                { `${recipe.nationality} - ${recipe.category}` }
+      {
+        copiedLink && <h2>Link copied!</h2>
+      }
+      {
+        doneRecipes.map((recipe, index) => (
+          <div key={ index }>
+            <Link
+              to={ `/${recipe.type}s/${recipe.id}` }
+              src={ recipe.image }
+            >
+              <img
+                src={ recipe.image }
+                alt={ recipe.name }
+                className="img"
+                data-testid={ `${index}-horizontal-image` }
+              />
+            </Link>
+
+            <div>
+              <div>
+                {
+                  recipe.type === 'food' ? (
+                    <p data-testid={ `${index}-horizontal-top-text` }>
+                      { `${recipe.nationality} - ${recipe.category}` }
+                    </p>
+                  )
+                    : (
+                      <p data-testid={ `${index}-horizontal-top-text` }>
+                        { recipe.category }
+                        { recipe.alcoholicOrNot }
+                      </p>
+                    )
+                }
+                <button
+                  type="button"
+                  onClick={ () => getURL(recipe) }
+                  className="btnURL"
+                >
+                  <img
+                    src={ shareIcon }
+                    alt="share"
+                    data-testid={ `${index}-horizontal-share-btn` }
+                    className="doneRecipesShare"
+                  />
+                </button>
+              </div>
+              <Link to={ `/${recipe.type}s/${recipe.id}` }>
+                <h3 data-testid={ `${index}-horizontal-name` }>
+                  {recipe.name}
+                </h3>
+              </Link>
+              <p data-testid={ `${index}-horizontal-done-date` }>
+                {`Done in: ${recipe.doneDate}`}
               </p>
-            )
-              : (
-                <p data-testid={ `${index}-horizontal-top-text` }>
-                  { recipe.alcoholicOrNot}
-                </p>
-              )
-          }
-          <img
-            data-testid={ `${index}-horizontal-image` }
-            src={ recipe.image }
-            alt={ recipe.name }
-            className="img"
-          />
-          <p
-            data-testid={ `${index}-horizontal-name` }
-          >
-            {recipe.name }
-          </p>
-          <p data-testid={ `${index}-horizontal-done-date` }>
-            {recipe.doneDate }
-          </p>
-          <img
-            data-testid={ `${index}-horizontal-share-btn` }
-            src={ shareIcon }
-            alt="icone de compartilhar"
-          />
-          <ul>
-            {recipe.tags.slice(0, 2).map((tagName, indexTag) => (
-              <li
-                key={ indexTag }
-                data-testid={ `${index}-${tagName}-horizontal-tag` }
-              >
-                {tagName}
-              </li>
-            ))}
-          </ul>
-        </div>
-      ))}
+              <ul>
+                {
+                  recipe.tags.slice(0, num).map((tag, indexTag) => (
+                    <li
+                      className="doneRecipesTag"
+                      key={ indexTag }
+                      data-testid={ `${index}-${tag}-horizontal-tag` }
+                    >
+                      {tag}
+                    </li>
+                  ))
+                }
+              </ul>
+            </div>
+          </div>
+        ))
+      }
     </section>
   );
 }
 
-export default CardDoneRecipesCards;
+DoneRecipesCards.propTypes = {
+  doneRecipes: PropTypes.arrayOf.isRequired,
+};
+
+export default DoneRecipesCards;
